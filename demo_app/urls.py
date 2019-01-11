f3ae629a289203f851e59c5cdffa8fada9d98615
katsuya1205 <katsuya2428@yahoo.co.jp>
2019-01-11 11:24:00 +0900

    add index.html

--- demo_app/urls.py
+++ demo_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views# 同じ階層のviews.pyを読み込む、という意味
 
 urlpatterns = [
-path("", views.index, name="index")
+    path("", views.index, name="index")
 ]# views.pyには様々な関数が入っている。関数indexを呼び出した。
--- demo_app/views.py
+++ demo_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render# render template(flask)のと同じ。HTMLファイルを読むときに使う
+from django.shortcuts import render
+# render template(flask)のと同じ。HTMLファイルを読むときに使う
 
 def index(request):
     return render(request, "demo_app/index.html", {})
+    # {}の中のものをHTML側に渡す
